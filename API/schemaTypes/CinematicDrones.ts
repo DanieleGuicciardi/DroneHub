a412e68da9a4690b72e7acabe71526cc0caa6f80
Daniele Guicciardi <dany.guicciardi@outlook.com>
2025-04-03 13:12:16 +0200

    API Initialization

---
+++ API/schemaTypes/CinematicDrones.ts
@@ -0,0 +1,54 @@
+import { defineField, defineType } from 'sanity'
+
+export const droneProductType = defineType({
+  name: 'CineDrones',
+  title: 'CineDrones',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Product',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      validation: (rule) => rule.required().min(1).precision(2),
+    }),
+    defineField({
+      name: 'image1',
+      title: 'Image 1',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'image2',
+      title: 'Image 2',
+      type: 'image',
+    }),
+    defineField({
+      name: 'image3',
+      title: 'Image 3',
+      type: 'image',
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [{ type: 'block' }],
+    }),
+    defineField({
+      name: 'specifications',
+      title: 'Spec',
+      type: 'text',
+      description: 'Add one specification per line using Enter.',
+    }),
+    defineField({
+      name: 'stock',
+      title: 'stock',
+      type: 'number',
+    })
+  ],
+})
---
+++ API/schemaTypes/index.ts
@@ -0,0 +1,4 @@
+import { droneProductType } from './CinematicDrones'
+
+export const schemaTypes = [droneProductType]
+
